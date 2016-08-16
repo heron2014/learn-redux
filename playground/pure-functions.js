490e337fdaa607d540afe749f3f492db6e970c42
Anita <a.nita@hotmail.co.uk>
2016-08-16 14:23:49 +0100

    install redux and play with pure functions

---
+++ playground/pure-functions.js
@@ -0,0 +1,46 @@
+// pure function
+function add(a, b) {
+  return a + b;
+}
+
+// non pure functions
+var a = 3;
+function add2(b) {
+  return a + b;
+}
+
+var result; // side effect
+function add3(a,b) {
+  result = a + b;
+  return result;
+}
+
+function add4(a, b) {
+  return a + b + new Date().getSeconds();
+}
+
+
+//pure function dont allow modify, update the passed values
+
+function changeProp(obj) {
+  // this code below would modify the starting obj
+  // obj.name = 'Aza';
+  // return obj;
+
+
+// this code will create new object
+  return {
+    ...obj,
+    name: 'Aza'
+  }
+}
+
+//we dont want to modify the starting obj
+var startingObj = {
+  name: 'Rocky',
+  age: 2
+}
+
+var res = changeProp(startingObj);
+console.log('starting', startingObj);
+console.log('res', res);
