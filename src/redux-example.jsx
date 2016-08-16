74a8ac09d70d8ff34730bbd99887ee23693df12b
Anita <a.nita@hotmail.co.uk>
2016-08-16 15:16:00 +0100

    dispatch first action

--- src/redux-example.jsx
+++ src/redux-example.jsx
@@ -4,10 +4,28 @@ console.log('starting redux example');
 
 var reducer = (state = {name: 'Annonymous'}, action) => {
   // state = state || {name: 'Anonymous'}; //default value
-  return state;
+  switch(action.type) {
+    case 'CHANGE_NAME':
+    return {
+      ...state,
+      name: action.name
+    };
+    default:
+    return state;
+  }
 }
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
-console.log('currentState', currentState); 
+console.log('currentState', currentState);
+
+//create action which is an object with prop type..
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Anita'
+}
+
+//dispatch action
+store.dispatch(action); //you can add action object dynamicllay in dispatch method
+console.log('name should be anita', store.getState());
--- src/redux-todo-example.jsx
+++ src/redux-todo-example.jsx
@@ -8,9 +8,29 @@ var stateDefault = {
 
 var reducer = (state = stateDefault, action) => {
 
-  return state;
+  switch(action.type) {
+  case 'CHANGE_SEARCH_TEXT':
+    return {
+      ...state,
+      searchText: action.searchText
+    };
+  default:
+    return state;
+  }
 }
 
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+//create action which is an object with prop type..
+var action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Walk a dog'
+}
+
+//dispatch action
+store.dispatch(action);
+
+console.log('new searchText is', store.getState());
